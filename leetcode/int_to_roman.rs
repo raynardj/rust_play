29517ba992a84fc551b0387184e9f95ccb3e0340
raynardj <raynard@rasenn.com>
2022-02-07 12:51:12 +0800

    🍒 lintered

--- leetcode/int_to_roman.rs
+++ leetcode/int_to_roman.rs
@@ -1,35 +1,35 @@
-pub fn to_roman(num:i32, digit:usize) -> String {
-    let letter_map=[
-            ['I', 'V', 'X'],
-            ['X', 'L', 'C'],
-            ['C', 'D', 'M'],
-            ['M', 'M', 'M']
-        ];
-    let chars = letter_map[digit as usize]; 
+pub fn to_roman(num: i32, digit: usize) -> String {
+    let letter_map = [
+        ['I', 'V', 'X'],
+        ['X', 'L', 'C'],
+        ['C', 'D', 'M'],
+        ['M', 'M', 'M'],
+    ];
+    let chars = letter_map[digit as usize];
 
     let mut result = String::new();
 
-        if num<=3 {
-            result = chars[0].to_string().repeat(num as usize);
-            result
-        } else if num==4 {
-            result = chars[0].to_string() + &chars[1].to_string();
-            result
-        } else if num==5 {
-            result = chars[1].to_string();
-            result
-        } else if num<=8 {
-            result = chars[1].to_string() + &chars[0].to_string().repeat(num as usize-5);
-            result
-        } else if num==9 {
-            result = chars[0].to_string() + &chars[2].to_string();
-            result
-        } else {
-            panic!("Invalid number");
-        }
+    if num <= 3 {
+        result = chars[0].to_string().repeat(num as usize);
+        result
+    } else if num == 4 {
+        result = chars[0].to_string() + &chars[1].to_string();
+        result
+    } else if num == 5 {
+        result = chars[1].to_string();
+        result
+    } else if num <= 8 {
+        result = chars[1].to_string() + &chars[0].to_string().repeat(num as usize - 5);
+        result
+    } else if num == 9 {
+        result = chars[0].to_string() + &chars[2].to_string();
+        result
+    } else {
+        panic!("Invalid number");
+    }
 }
 
-pub fn digit_array(num:i32) -> Vec<i32> {
+pub fn digit_array(num: i32) -> Vec<i32> {
     // return a vector of digits
     let mut digits: Vec<i32> = Vec::new();
     let mut num = num;
