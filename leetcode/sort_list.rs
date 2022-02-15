8e5cd2d903ce3cdcfdf619bb3d23df4548f84289
raynardj <raynard@rasenn.com>
2022-02-15 13:31:38 +0800

    🐺 fastest

--- leetcode/sort_list.rs
+++ leetcode/sort_list.rs
@@ -22,26 +22,25 @@
 // Runtime: 1657 ms, faster than 100.00% of Rust online submissions for Sort List.
 // Memory Usage: 5.4 MB, less than 100.00% of Rust online submissions for Sort List.
 impl Solution {
-    pub fn sort_list_rec(mut head: Option<Box<ListNode>>, recalc:bool) -> Option<Box<ListNode>> {
+    pub fn sort_list_rec(mut head: Option<Box<ListNode>>, recalc: bool) -> Option<Box<ListNode>> {
         /* recalc:recalculate sorting the h.next */
         match (head) {
             Some(mut h) => {
                 if h.next.is_some() {
-                    let mut t:Box<ListNode>;
-                    if recalc{
+                    let mut t: Box<ListNode>;
+                    if recalc {
                         t = Self::sort_list_rec(h.next, true).unwrap();
                     } else {
                         t = h.next.unwrap();
                     }
-                    
                     if h.val > t.val {
                         h.next = t.next;
                         /*not recalculating*/
-                        t.next = Self::sort_list_rec(Some(h),false);
+                        t.next = Self::sort_list_rec(Some(h), false);
                         Some(t)
                     } else {
                         // this step is necessary, to avoid borrow error
-                        h.next =Some(t);
+                        h.next = Some(t);
                         Some(h)
                     }
                 } else {
